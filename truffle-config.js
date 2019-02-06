338ddc1a8a856d8d5ff95c0f871be5dbdf14a5d7
Sebastinez <7912302+Sebastinez@users.noreply.github.com>
2019-02-06 16:24:48 -0300

    Changes to MVP

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,29 @@
+var HDWalletProvider = require('truffle-hdwallet-provider')
+
+var mnemonic = '7a0824e86e5c362c523d7f4991de30b56a9c04f653c33573b0a1e3b8850b23c6'
+var publicNode = 'https://public-node.testnet.rsk.co:443'
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 
     networks: {
-      development: {
+      aws: {
          host: "3.17.167.226",
          port: 8545,
-         network_id: "*"
-      }
+         network_id: "*",
+      },
+      rsk: {
+        provider: () =>
+        new HDWalletProvider(mnemonic, publicNode),
+        network_id: "*",
+        gas: 2500000,
+        gasPrice: 183000
+     },
+     dev: {
+       host: "127.0.0.1",
+       port: 8545,
+       network_id: "*"
+     }
     }
 };
--- webapp/MOWebLib.js
+++ webapp/MOWebLib.js
@@ -1,123 +1,142 @@
 var contract = undefined;
 var customProvider = undefined;
-var address = "0x5C83dd6941A707E460f3887a8D1fca880ffE4E29";
-var account = "0xccfde609e29d6a8d5a1ce5b518d131d5e2254cb4";
-var privatekey = "0x066407055f594cfc05938e8edcf6930362c39b0bd3900e40b2df76c18fdf4178";
+var address = "0x2AA7cA402893A1f25E450cB3dB303e1F72a853b4";
+var account = "0x1c410dbe71e5f178f5543f91a0e757c450d907fc";
+var privatekey = "0x778a33744bb33d933161ee75e6321d0d758130b16e48cb0629505bab1b4e650f";
 var abi = undefined;
 
-function mo_init () {
-  // Check if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    // Use existing gateway
-    //window.web3 = new Web3(web3.currentProvider);
+function mo_init (account,privatekey) {
+	// Check if Web3 has been injected by the browser (Mist/MetaMask)
+	if (typeof web3 !== 'undefined') {
+		// Use existing gateway
+		//window.web3 = new Web3(web3.currentProvider);
 
-    let provider = new Web3.providers.HttpProvider("http://3.17.167.226:8545");
-    web3 = new Web3(provider);
-  } else {
-   alert("No Ethereum interface injected into browser. Read-only access");
-  }
+		let provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
+		web3 = new Web3(provider);
+	} else {
+	 alert("No Ethereum interface injected into browser. Read-only access");
+	}
 
-  abi = [
-    {
-      "inputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "hash",
-          "type": "bytes32"
-        }
-      ],
-      "name": "addDocHash",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [
-        {
-          "name": "hash",
-          "type": "bytes32"
-        }
-      ],
-      "name": "findDocHash",
-      "outputs": [
-        {
-          "name": "",
-          "type": "uint256"
-        },
-        {
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ];
+	abi = [
+		{
+			"inputs": [],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "constructor",
+			"signature": "constructor"
+		},
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"name": "hash",
+					"type": "bytes32"
+				}
+			],
+			"name": "addDocHash",
+			"outputs": [],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function",
+			"signature": "0x133a48f3"
+		},
+		{
+			"constant": true,
+			"inputs": [
+				{
+					"name": "hash",
+					"type": "bytes32"
+				}
+			],
+			"name": "findDocHash",
+			"outputs": [
+				{
+					"name": "",
+					"type": "uint256"
+				},
+				{
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function",
+			"signature": "0x4cf00d3f"
+		}
+	];
 
-  contract = new web3.eth.Contract(abi, address);
-  web3.eth.personal.unlockAccount(account,privatekey,100).then(function(log){console.log("Account unlocked")});
-  web3.eth.getAccounts(function (error, accounts) {
-  $("#account").html(accounts[0]);
-    web3.eth.getBalance(accounts[0]).then(
-      function(ether){
-        let url = "https://api.coinmarketcap.com/v1/ticker/ethereum/";
-        fetch(url)
-        .then(function(response) {
-        return response.json();
-        })
-        .then(function(data) {
-        let eth1 = JSON.stringify(data[0]);
-        let eth2 = JSON.parse(eth1);
-        $("#ether").html(" con " + Math.round((ether/10**18)*100)/100 + " ETH / " + Math.round((ether/10**18 * eth2.price_usd)*100)/100 + " USD")})
-        })
-  })
+	contract = new web3.eth.Contract(abi, address);
+	//web3.eth.personal.unlockAccount(account,privatekey,100).then(function(log){console.log("Account unlocked")});
+	web3.eth.getAccounts(function (error, accounts) {
+	$("#account").html(accounts[0]);
+		web3.eth.getBalance(accounts[0]).then(
+			function(ether){
+				let url = "https://api.coinmarketcap.com/v1/ticker/ethereum/";
+				fetch(url)
+				.then(function(response) {
+				return response.json();
+				})
+				.then(function(data) {
+				let eth1 = JSON.stringify(data[0]);
+				let eth2 = JSON.parse(eth1);
+				$("#ether").html(" con " + Math.round((ether/10**18)*100)/100 + " ETH / " + Math.round((ether/10**18 * eth2.price_usd)*100)/100 + " USD")})
+				})
+	})
 };
 
 //sends a hash to the blockchain
+/*function MO_send(hash, callback) {
+		web3.eth.getAccounts(function (error, accounts) {
+			contract.methods.addDocHash(hash).send({
+				from: accounts[0]
+			},function(error, tx) {
+				if (error) callback(error, null);
+				else callback(null, tx);
+			});
+
+			web3.eth.getAccounts(function (error, accounts) {
+
+			let url = "https://api.coinmarketcap.com/v1/ticker/ethereum/";
+			fetch(url)
+			.then(function(response) {
+			return response.json();
+			})
+			.then(function(data) {
+			let eth1 = JSON.stringify(data[0]);
+			let eth2 = JSON.parse(eth1);
+			web3.eth.getBalance(accounts[0]).then(function(ether){$("#ether").html(" con " + Math.round((ether/10**18)*100)/100 + " ETH / " + Math.round((ether/10**18 * eth2.price_usd)*100)/100 + " USD")});
+			})
+			})
+		});
+};*/
+
 function MO_send(hash, callback) {
-    web3.eth.getAccounts(function (error, accounts) {
-      contract.methods.addDocHash(hash).send({
-        from: accounts[0]
-      },function(error, tx) {
-        if (error) callback(error, null);
-        else callback(null, tx);
-      });
+	var _tx = undefined;
+	web3.eth.getAccounts(function (error, accounts) {
+		contract.methods.addDocHash(hash).send({from: accounts[0]})
+		.on('transactionHash', (tx) => {
+		})
+		.on('receipt', (receipt) => {
+			callback(null, receipt);
+		})
+		.on('confirmation', (confirmationNumber, receipt) => {
 
-      web3.eth.getAccounts(function (error, accounts) {
-
-      let url = "https://api.coinmarketcap.com/v1/ticker/ethereum/";
-      fetch(url)
-      .then(function(response) {
-      return response.json();
-      })
-      .then(function(data) {
-      let eth1 = JSON.stringify(data[0]);
-      let eth2 = JSON.parse(eth1);
-      web3.eth.getBalance(accounts[0]).then(function(ether){$("#ether").html(" con " + Math.round((ether/10**18)*100)/100 + " ETH / " + Math.round((ether/10**18 * eth2.price_usd)*100)/100 + " USD")});
-      })
-      })
-    });
+			})
+		.on('error', console.error);
+});
 };
 
 //looks up a hash on the blockchain
 function MO_find (hash, callback) {
-  contract.methods.findDocHash(hash).call( function (error, result) {
-    if (error) callback(error, null);
-    else {
-      let resultObj = {
-        mineTime:  new Date(result[0] * 1000),
-        blockNumber: result[1]
-      }
-      callback(null, resultObj);
-    }
-  });
+	contract.methods.findDocHash(hash).call( function (error, result) {
+		if (error) callback(error, null);
+		else {
+			let resultObj = {
+				mineTime:  new Date(result[0] * 1000),
+				blockNumber: result[1]
+			}
+			callback(null, resultObj);
+		}
+	});
 };
