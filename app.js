a84c71d3894d42f01956229a232cc0de8b30af27
Sebastian Martinez <sebastinez@me.com>
2019-02-17 09:10:58 -0300

    Improving of file upload and auth

--- app.js
+++ app.js
@@ -17,19 +17,18 @@ app.use(
 	require('express-session')({
 		secret: 'oomph quant brake linseed vitrics deicide abandon piping playboy yataghan',
 		resave: false,
-		saveUninitialized: false
+		saveUninitialized: false,
+		cookie: { secure: false }
 	})
 );
 
 var upload = multer({ dest: 'uploads/' });
 
-var hash = crypto.createHash('sha256');
-
 // Require and connect to mongoose database
 mongoose.connect('mongodb://localhost/mo_nrs', { useNewUrlParser: true });
 
 var contract = undefined;
-var address = '0x7af24F98a3Ad5c651eA7925482dE8e0f910c965f';
+var address = '0xc4a0C8E1Cf512a931E4d8fAA8a6FC014864b9f3e';
 
 const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
 
@@ -68,13 +67,13 @@ function mo_init() {
 
 mo_init();
 
-function MO_send(hash, callback) {
+function MO_send(hash, docid, user, callback) {
 	web3.eth.getAccounts(function(error, accounts) {
 		contract.methods
 			.addDocHash(hash)
 			.send({ from: accounts[0] })
 			.on('transactionHash', (tx) => {
-				Document.create({ id: 'Test', hash: hash, tx: tx, mined: false }, function(err, doc) {
+				Document.create({ id: docid, hash: hash, tx: tx, mined: false,username: user }, function(err, doc) {
 					if (err) {
 						console.log('Something went wrong');
 					} else {
@@ -126,30 +125,31 @@ function isLoggedIn(req, res, next) {
 //=======================
 
 app.get('/', function(req, res) {
-	res.render('home');
+	if (req.isAuthenticated()) {
+		res.render('home_logged', { username: req.session.user });
+	} else {
+		res.render('home');
+	}
 });
 
-app.get('/hash', function(req, res) {
-	res.render('hash');
+app.get('/hash', isLoggedIn, function(req, res) {
+	res.render('hash_logged', { username: req.session.user });
 });
 
-app.get('/list', function(req, res) {
+app.get('/list', isLoggedIn, function(req, res) {
 	var list = undefined;
-	Document.find({}, function(err, doc) {
+	Document.find({username:req.session.user}, function(err, doc) {
 		if (err) {
 			console.log('Something went wrong');
 		} else {
-			res.render('list', { list: doc });
+				res.render('list_logged', { list: doc, username: req.session.user });
+	
 		}
 	});
 });
 
 app.get('/signup', function(req, res) {
 	res.render('signup');
-});
-
-app.get('/secret', isLoggedIn, function(req, res) {
-	res.render('secret');
 });
 
 app.get('/login', function(req, res) {
@@ -161,32 +161,46 @@ app.get('/logout', function(req, res) {
 	res.redirect('/');
 });
 
-app.post(
-	'/login',
-	upload.none(),
-	passport.authenticate('local', {
-		successRedirect: '/secret',
-		failureRedirect: '/login'
-	}),
-	function(req, res) {}
-);
+app.post('/login', upload.none(), function(req, res, next) {
+	passport.authenticate('local', function(err, user) {
+		if (!user) {
+			return res.redirect('/login');
+		}
+		if (err) {
+			console.log(err);
+		}
+		req.logIn(user, function(err) {
+			if (err) {
+				return next(err);
+			}
+		});
+		req.session.user = user.username;
+		return res.render('home_logged', {
+			username: req.session.user
+		});
+	})(req, res, next);
+});
 
 app.post('/signup', upload.none(), function(req, res) {
-	console.log(req.body);
 	User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
 		if (err) {
 			console.log(err);
 			return res.render('signup');
 		}
 		passport.authenticate('local')(req, res, function() {
-			res.render('secret');
+			req.session.user = req.body.username;
+			res.render('home_logged',{username:req.session.user});
 		});
 	});
 });
 
 app.post('/hash', upload.single('newhash'), function(req, res, next) {
+	var docid = req.body.id;
 	// creates a read stream of the uploaded file through multer
 	var fd = fs.createReadStream(req.file.path);
+
+	var hash = crypto.createHash('sha256');
+
 	// Defines the encoding as Hex
 	hash.setEncoding('hex');
 
@@ -198,30 +212,21 @@ app.post('/hash', upload.single('newhash'), function(req, res, next) {
 		// When the Read Stream ends, end the hash object and read the obtained hash
 		hash.end();
 		var hashed = hash.read();
-		fd.unpipe(hash);
 
 		console.log('0x' + hashed);
 
 		MO_find('0x' + hashed, function(error, resultObj) {
 			if (resultObj.blockNumber != 0) {
-				res.render('partials/check_duplicate', { hashed: hashed, result: resultObj });
+				res.render('partials/check_duplicate', { hashed: hashed, docid: docid, result: resultObj, username:req.session.user});
 			} else {
 				//Call MO_send function with the hex hash and await the result for rendering the hash page
-				MO_send('0x' + hashed, function(error, result) {
+				MO_send('0x' + hashed, docid, req.session.user, function(error, result) {
 					if (result) {
-						res.render('partials/hash', { hashed: hashed, result: result });
+						res.render('partials/hash', { hashed: hashed, docid: docid, result: result,username:req.session.user});
 					}
 				});
 			}
 		});
-	});
-
-	fd.on('error', function() {
-		console.log('Error');
-	});
-
-	fd.on('unpipe', function() {
-		console.log('unpiped');
 	});
 
 	// read all file and pipe it (write it) to the hash object
@@ -257,15 +262,11 @@ app.post('/check', upload.single('newhash'), function(req, res, next) {
 		//Call MO_find function with the hex hash and await the result for rendering the check hash page
 		MO_find('0x' + hashed, function(error, resultObj) {
 			if (resultObj.blockNumber != 0) {
-				res.render('partials/check', { hashed: hashed, result: resultObj });
+				res.render('partials/check', { hashed: hashed, result: resultObj,username:req.session.user });
 			} else {
-				res.render('partials/check_notfound', { hashed: hashed });
+				res.render('partials/check_notfound', { hashed: hashed,username:req.session.user });
 			}
 		});
-	});
-
-	fd.on('error', function() {
-		console.log(error);
 	});
 
 	// read all file and pipe it (write it) to the hash object
--- models/document.js
+++ models/document.js
@@ -4,7 +4,8 @@ var docSchema = new mongoose.Schema({
 	id: String,
 	hash: String,
 	tx: String,
-	mined: Boolean
+	mined: Boolean,
+	username: String
 });
 
 module.exports = mongoose.model('Document', docSchema);
