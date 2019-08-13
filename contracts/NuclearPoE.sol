c4c6c583f9326e628144ba794aa90fe756ac7063
Sebastinez <sebastinez@me.com>
2019-08-13 17:11:24 -0300

    Implementing safe guards in Smart Contract

---
+++ contracts/NuclearPoE.sol
@@ -0,0 +1,81 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+/// @title A contract to maintain the records for the future of the nuclear industry
+/// @author Sebastian Martinez
+/// @notice You can use this contract for saving hashes and aditional information about mechanical documentation.
+/// @dev All function calls are currently implemented without side effects
+contract NuclearPoE {
+
+    bytes32[] public documents;
+    address public owner;
+
+    struct Document {
+        address user;
+        uint32 mineTime;
+        uint256 blockNumber;
+        bytes32 title;
+        bytes32 storageHash;
+        uint8 storageFunction;
+        uint8 storageSize;
+    }
+
+    struct Supplier {
+        address direction;
+        bytes32 name;
+    }
+
+    struct Client {
+        address direction;
+        bytes32 name;
+    }
+
+    mapping (bytes32 => Document) private document;
+    mapping(address => Supplier) public suppliers;
+    mapping(address => Client) public clients;
+
+    constructor() public {
+      owner = msg.sender;
+    }
+
+    modifier onlySupplier() {
+    require(
+        msg.sender == owner,
+        "Only supplier can upload documentation"
+    );
+    _;
+    }
+
+    modifier onlyOwner() {
+    require(
+        msg.sender == owner,
+        "Only owner can make this change"
+    );
+    _;
+    }
+
+    /// @notice Creates a new entry in the documents
+    /// @param hash of document, timestamp of operation, title of document, hash of document storage, function of document storage, size of storage
+    function addDocHash (bytes32 hash, uint32 mineTime, bytes32 titulo, bytes32 storageHash, uint8 storageFunction, uint8 storageSize) public onlySupplier {
+        document[hash] = Document(msg.sender, mineTime, block.number, titulo, storageHash, storageFunction, storageSize);
+        documents.push(hash);
+    }
+
+    function getDocuments() public view onlyOwner returns (bytes32[] memory) {
+        return documents;
+    }
+
+    function addSupplier(address direction, bytes32 name) public {
+      suppliers[direction] = Supplier(direction, name);
+    }
+
+    function findDocHash (bytes32 hash) public view returns(address,uint32, uint256, bytes32, bytes32, uint8, uint8) {
+        return (document[hash].user,
+        document[hash].mineTime,
+        document[hash].blockNumber,
+        document[hash].title,
+        document[hash].storageHash,
+        document[hash].storageFunction,
+        document[hash].storageSize);
+    }
+
+}
