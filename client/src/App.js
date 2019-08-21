8d4fa092f93383e77e28b419ccd79c78559479ae
Sebastinez <sebastinez@me.com>
2019-08-21 15:44:07 -0300

    Start with a fresh react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,12 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "semantic-ui-less/semantic.less";
-import Navbar from "./components/Navbar";
-import Index from "./components/Index";
-import { Container } from "semantic-ui-react";
-import Subir from "./components/Subir";
-import Chequear from "./components/Chequear";
-import Acceder from "./components/Acceder";
-import Listado from "./components/Listado";
-import Progress from "./components/Progress";
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <Container style={{ marginTop: "1em" }}>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Index} />
-        <Route exact path="/listado" component={Listado} />
-        <Route exact path="/subir" component={Subir} />
-        <Route exact path="/chequear" component={Chequear} />
-        <Route exact path="/acceder" component={Acceder} />
-        <Route exact path="/progress" component={Progress} />
-      </Switch>
-    </Container>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">NUCLEARIS</header>
+        </div>
+    )
 }
 
-export default App;
+export default App
