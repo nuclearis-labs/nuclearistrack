d6c8b2685c1e659024c6d7e97599880763dd9b79
Sebastinez <sebastinez@me.com>
2019-08-23 17:19:09 -0300

    Error handling

--- routes/proyecto.js
+++ routes/proyecto.js
@@ -3,19 +3,21 @@ let express = require('express'),
     router = express.Router({ mergeParams: true })
 
 router.post('/', async (req, res) => {
-    let proyecto = new Blockchain()
-    await proyecto.addProject(
-        Number(req.body.expediente),
-        req.body.proyectoTitle.toString(),
-        req.body.clientAddress.toString(),
-        req.body.clientName.toString()
-    )
-    let tx = await proyecto.sendTx()
-    res.json({ message: `Project created`, data: tx })
+    let proyecto = new Blockchain(req.body.keys)
+    try {
+        await proyecto.addProject(
+            Number(req.body.expediente),
+            req.body.proyectoTitle.toString(),
+            req.body.clientAddress.toString(),
+            req.body.clientName.toString()
+        )
+        let tx = await proyecto.sendTx()
+        res.json({ message: `Project created`, data: tx })
+    } catch (e) { res.json({ error: e.message }) }
 })
 
 router.post('/approve', async (req, res) => {
-    let proyecto = new Blockchain()
+    let proyecto = new Blockchain(req.body.keys)
     try {
         await proyecto.approveProject(Number(req.body.expediente))
         let tx = await proyecto.sendTx()
