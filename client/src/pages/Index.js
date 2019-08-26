415edbd597f030826581d181c9ee5a9d1b1c0d71
Sebastinez <sebastinez@me.com>
2019-08-26 15:33:44 -0300

    Router and first form upload to client

---
+++ client/src/pages/Index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Index() {
+    return (<div class="hero">
+        Primer plataforma descentralizada de trazabilidad con tecnologia
+        Blockchain para la industria nuclear
+</div>)
+}
