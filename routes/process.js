33ff8bd0e1230eb0a22bfedda516b8a177cfabe0
Sebastian Martinez <7912302+sebastinez@users.noreply.github.com>
2019-09-02 17:22:10 -0300

    process creation route (#15)

--- routes/process.js
+++ routes/process.js
@@ -1,33 +1,20 @@
-const express = require('express');
-const Blockchain = require('../classes/Blockchain');
-
-const router = express.Router({ mergeParams: true });
+let express = require('express'),
+    Blockchain = require('../classes/Blockchain'),
+    router = express.Router({ mergeParams: true })
 
 router.post('/', async (req, res) => {
-  const proyecto = new Blockchain(req.body.keys);
-  try {
-    await proyecto.addProject(
-      Number(req.body.expediente),
-      req.body.proyectoTitle.toString(),
-      req.body.clientAddress.toString(),
-      req.body.clientName.toString()
-    );
-    const tx = await proyecto.sendTx();
-    res.json({ message: `Project created`, data: tx });
-  } catch (e) {
-    res.json({ error: e.message });
-  }
-});
+    let process = new Blockchain(req.body.keys)
+    try {
+        await process.addProcess(
+            Number(req.body.expediente),
+            req.body.supplierAddress,
+            req.body.processTitle,
+            req.body.supplierName
+        )
+        let tx = await process.sendTx()
+        res.json({ message: `Process created`, data: tx })
+    } catch (e) { res.json({ error: e.message }) }
+})
 
-router.post('/approve', async (req, res) => {
-  const proyecto = new Blockchain(req.body.keys);
-  try {
-    await proyecto.approveProject(Number(req.body.expediente));
-    const tx = await proyecto.sendTx();
-    res.json({ message: `Project approved`, data: tx });
-  } catch (e) {
-    res.json({ error: e.message });
-  }
-});
 
-module.exports = router;
+module.exports = router
