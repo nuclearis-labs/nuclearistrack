fb25ed3f975bf0e87d02c6b1065edd376b5fd045
Sebastinez <sebastinez@me.com>
2019-11-12 14:29:01 -0300

    Removed CRA Typescript

--- src/config/ipfs.ts
+++ src/config/ipfs.ts
@@ -1,8 +1,16 @@
 import ipfs from 'ipfs-http-client';
 
-const node = new ipfs('localhost', '5001', {
-  protocol: 'http'
-});
+let host: string;
+let port: string;
+if (process.env.NODE_ENV === 'development') {
+  host = 'localhost';
+  port = '5001';
+} else {
+  host = 'ec2-18-229-161-21.sa-east-1.compute.amazonaws.com';
+  port = '5001';
+}
+
+const node = new ipfs(host, port, { protocol: 'http' });
 
 export async function saveToIPFS(buffer: Buffer) {
   try {
