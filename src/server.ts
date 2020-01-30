9ae851316a7316e8fdf3431964c53ab518b64d3e
Sebastinez <sebastinez@me.com>
2020-01-30 15:07:33 -0300

    Change to dev and prod

--- src/server.ts
+++ src/server.ts
@@ -23,15 +23,11 @@ app.use('/api/user', require('./routes/user'));
 app.use('/api/transfer', require('./routes/transfer'));
 app.use('/auth', require('./routes/auth'));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-    );
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+});
 
 app
   .listen(process.env.PORT, () =>
