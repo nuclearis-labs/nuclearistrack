2d9a765b8bb779ba705a8eaedf4f278bf2a8a483
sebastinez <sebastinez@me.com>
2020-03-27 11:09:37 -0300

    Simplified Truffle Config

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,6 @@
 require('ts-node/register');
 require('dotenv').config();
 
-const HDWalletProvider = require('truffle-hdwallet-provider');
-
 const testNode = 'https://public-node.testnet.rsk.co:443';
 const publicNode = 'https://public-node.rsk.co';
 
@@ -11,20 +9,6 @@ module.exports = {
   // to customize your Truffle configuration!
 
   networks: {
-    testnet: {
-      provider: () =>
-        new HDWalletProvider(process.env.PRIVKEY_TESTNET, testNode),
-      network_id: '*',
-      gas: 4000000,
-      gasPrice: 59240000
-    },
-    mainnet: {
-      provider: () =>
-        new HDWalletProvider(process.env.PRIVKEY_MAINNET, publicNode),
-      network_id: '30',
-      gas: 6800000,
-      gasPrice: 60240000
-    },
     dev: {
       host: 'localhost',
       port: 8545,
