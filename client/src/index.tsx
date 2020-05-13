33d9eb8512af457629cac0e7de7b604534bf4e23
Sebastinez <sebastinez@me.com>
2020-05-13 14:31:41 -0300

    Comment out Redux Dev Tools in applyMiddleware

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,11 +12,11 @@ import logger from 'redux-logger';
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk, logger),
+    applyMiddleware(thunk, logger)
     // @ts-ignore "Devtools hook into the window object"
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      // @ts-ignore "Devtools hook into the window object"
-      window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })
+    //   window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //     // @ts-ignore "Devtools hook into the window object"
+    //     window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true, traceLimit: 25 })
   )
 );
 
