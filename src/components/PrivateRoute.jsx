4950d608b18cc730d1f3f95a94c60e0eac52e722
Sebastinez <sebastinez@me.com>
2020-07-15 16:12:23 -0300

    fixes a redirection when initializing web3

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,27 +1,32 @@
 import React, { useContext, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
+import { useTimeout } from 'react-use';
+import Loading from '../pages/Loading';
 
 function PrivateRoute({ children, ...rest }) {
   const { isReady, isConnected, connect } = useContext(UserContext);
+  const [timePassed] = useTimeout(1500);
 
   useEffect(() => {
     if (isReady) connect();
     // eslint-disable-next-line
   }, [isReady]);
 
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        isConnected ? (
-          children
-        ) : (
-          <Redirect to={{ pathname: '/login', state: { from: location } }} />
-        )
-      }
-    />
-  );
+  if (timePassed())
+    return (
+      <Route
+        {...rest}
+        render={({ location }) =>
+          isConnected ? (
+            children
+          ) : (
+            <Redirect to={{ pathname: '/login', state: { from: location } }} />
+          )
+        }
+      />
+    );
+  else return <Loading />;
 }
 
 export default PrivateRoute;
---
+++ src/pages/Loading.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ReactComponent as Loader } from '../img/puff.svg';
+
+export default function Loading() {
+  return <Loader style={{ margin: '20vh 50vw 20vh 50vw' }} />;
+}
