761341bbe67be26a1bdd270485cc5f1ff97b4c31
Sebastinez <sebastinez@me.com>
2020-07-15 16:12:51 -0300

    fixes React Lint Errors

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,10 +9,10 @@ import {
 } from '../styles/webComponents';
 import { useHistory } from 'react-router';
 import { UserContext } from '../context/UserContext';
-import { useTranslation, Trans } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 export default function Login() {
-  const { t } = useTranslation(['header']);
+  const { t } = useTranslation();
   const history = useHistory();
   const { isLoading, isReady, isConnected, connect, account } = useContext(
     UserContext
@@ -54,15 +54,32 @@ export default function Login() {
           <ItemTit>{t('login:requirements')}</ItemTit>
           <ItemDesc>{t('login:requirementsText')}</ItemDesc>
           <ItemTit>{t('login:extensions')}</ItemTit>
-          <ItemDesc>
-            <Trans>login:extensionsText</Trans>
-          </ItemDesc>
+          <ItemDesc
+            dangerouslySetInnerHTML={{
+              __html: t('login:extensionsText', {
+                extensiones:
+                  "<ul style={{ paddingTop: '10px' }}><li>Metamask <a href='https://metamask.io/'>(Pagina Web)</a></li><li>Nifty<a href='https://www.poa.network/for-users/nifty-wallet'>(Pagina Web)</a></li></ul>",
+              }),
+            }}
+          />
           <ItemTit>{t('login:proximosPasos')}</ItemTit>
-          <ItemDesc>
-            <Trans>login:proximosPasosText</Trans>
-          </ItemDesc>
+          <ItemDesc
+            dangerouslySetInnerHTML={{
+              __html: t('login:proximosPasosText', {
+                requestEmail:
+                  "<a href='mailto:sistemas@nuclearis.com?subject=Request%20to%20join%20Nuclearistrack&body=I%20hereby%20ask%20you%20to%20give%20my%20company%20access%20to%20Nuclearistrack.%0D%0A%0D%0ACompany%20Name%3A%20%3CTo%20be%20filled%20out%3E%0D%0AHexadecimal%20Account%20Name%20on%20Metamask%3A%20%3CTo%20be%20filled%20out%3E%0D%0A%0D%0AThank%20you%20very%20much!'>sistemas@nuclearis.com</a>",
+              }),
+            }}
+          />
           <ItemTit>{t('login:soporte')}</ItemTit>
-          <ItemDesc>{t('login:soporteText')}</ItemDesc>
+          <ItemDesc
+            dangerouslySetInnerHTML={{
+              __html: t('login:soporteText', {
+                email:
+                  "<a href='mailto:sistemas@nuclearis.com?subject=Soporte%20NUCLEARISTRACK'>sistemas@nuclearis.com</a>",
+              }),
+            }}
+          ></ItemDesc>
         </WidthContent>
         <BottomSpace />
       </>
