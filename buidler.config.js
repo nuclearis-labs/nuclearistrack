39a018597d3abd78a530abd2cb37d12580ec61b2
Sebastian <sebastinez@me.com>
2020-08-24 12:11:04 -0300

    fix buidler config and env variables

--- buidler.config.js
+++ buidler.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './.env.local' });
+require('dotenv').config({ path: './.env' });
 usePlugin('@nomiclabs/buidler-truffle5');
 usePlugin('@nomiclabs/buidler-web3');
 usePlugin('solidity-coverage');
